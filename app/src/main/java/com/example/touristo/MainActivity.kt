6f9593cc2cdfe0f06ad4c07cde2831dcde84e51c
Mosh <qwwerrrty11@gmail.com>
2023-03-13 10:52:35 +0530

    initializer

---
+++ app/src/main/java/com/example/touristo/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.touristo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        window.decorView.apply {
+//            systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+//        }
+
+    }
+}
---
+++ app/src/main/java/com/example/touristo/UserIndex.kt
@@ -0,0 +1,56 @@
+package com.example.touristo
+
+import android.graphics.drawable.ColorDrawable
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.example.touristo.Fragments.BookingFrag
+import com.example.touristo.Fragments.CartFrag
+import com.example.touristo.Fragments.ProfileFrag
+import com.example.touristo.Fragments.UserHomeFrag
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class UserIndex : AppCompatActivity() {
+    private lateinit var btnNav : BottomNavigationView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_index)
+
+        //the if block is executed so that the notification pannel color changes and the Icon of them changes
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.statusBarColor = ContextCompat.getColor(this, R.color.bgBackground)
+
+            val flags = window.decorView.systemUiVisibility
+            window.decorView.systemUiVisibility = flags or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        }
+
+        btnNav = findViewById(R.id.bottomNavigationView)
+        replaceFragment(UserHomeFrag())
+        btnNav.setOnItemSelectedListener{
+            when(it.itemId){
+                R.id.home ->replaceFragment(UserHomeFrag())
+                R.id.cart ->replaceFragment(CartFrag())
+                R.id.booked ->replaceFragment(BookingFrag())
+                R.id.profile ->replaceFragment(ProfileFrag())
+
+                else->{
+
+                }
+            }
+            true
+        }
+
+    }
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.indexFrameLayout,fragment)
+        fragmentTransaction.commit()
+
+        
+    }
+}
