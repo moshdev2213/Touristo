048e11115a3cc4334e785a407cfb15d3fe11ac07
Mosh <qwwerrrty11@gmail.com>
2023-03-17 21:28:19 +0530

    doneAdmin

---
+++ app/src/main/java/com/example/touristo/AdminUpdateProfile.kt
@@ -0,0 +1,11 @@
+package com.example.touristo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AdminUpdateProfile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin_update_profile)
+    }
+}
