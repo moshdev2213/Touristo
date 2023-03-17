e16425030b51b4fd8b5d8d97f44246a40d671cfe
Mosh <qwwerrrty11@gmail.com>
2023-03-17 20:42:06 +0530

    almostAdmin

--- app/src/main/java/com/example/touristo/AdminHome.kt
+++ app/src/main/java/com/example/touristo/AdminHome.kt
@@ -6,21 +6,21 @@ import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.content.ContextCompat
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
-import com.example.touristo.Fragments.AdminHomeFrag
-import com.example.touristo.Fragments.AdminManagement
-import com.example.touristo.Fragments.TouristManagement
-import com.example.touristo.Fragments.VillaManagement
+import com.example.touristo.Fragments.*
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationView
 
 class AdminHome : AppCompatActivity() {
 lateinit var toggle : ActionBarDrawerToggle
 lateinit var drawerLayout : DrawerLayout
+private lateinit var btnNav : BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_home)
@@ -57,6 +57,31 @@ lateinit var drawerLayout : DrawerLayout
                 R.id.nav_customerManagement->replaceFrags(TouristManagement())
                 R.id.nav_adminManagement->replaceFrags(AdminManagement())
                 R.id.nav_villaManagement->replaceFrags(VillaManagement())
+                R.id.nav_profile->{
+                    replaceAdmBottomNavFragment(AdminBtmProfileFrag())
+                    drawerLayout.closeDrawers()
+
+                }
+                R.id.nav_logout->{
+                    Toast.makeText(this,"Implementation In Process \uD83D\uDE0E",Toast.LENGTH_SHORT).show()
+                    drawerLayout.closeDrawers()
+                }
+            }
+            true
+        }
+        //the below starts for the bottomNavgation parts
+        btnNav = findViewById(R.id.btmnavadmin)
+        btnNav.setOnItemSelectedListener{
+            when(it.itemId){
+                R.id.admprofile->{
+                    replaceAdmBottomNavFragment(AdminBtmProfileFrag())
+                }
+                R.id.admnotify->{
+                    replaceAdmBottomNavFragment(AdminBtmNotifyFrag())
+                }
+                R.id.admhome->{
+                    replaceFrags(AdminHomeFrag())
+                }
             }
             true
         }
@@ -64,6 +89,7 @@ lateinit var drawerLayout : DrawerLayout
 
     }
 
+    //the below function is for the frag replacement in the sidenavigation bar
     fun replaceFrags(fragment: Fragment){
         val fragmentManager = supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
@@ -76,5 +102,15 @@ lateinit var drawerLayout : DrawerLayout
             return true
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    //the below method replaces the fragments that are for the bottom navigation
+    private fun replaceAdmBottomNavFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout,fragment)
+        fragmentTransaction.commit()
+
+
     }
 }
--- app/src/main/java/com/example/touristo/RegisterAdmin.kt
+++ app/src/main/java/com/example/touristo/RegisterAdmin.kt
@@ -1,11 +1,22 @@
 package com.example.touristo
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.content.ContextCompat
 
 class RegisterAdmin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_admin)
+
+        //the if block is executed so that the notification pannel color changes and the Icon of them changes
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.statusBarColor = ContextCompat.getColor(this, R.color.bgBackground)
+
+            val flags = window.decorView.systemUiVisibility
+            window.decorView.systemUiVisibility = flags or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        }
     }
 }
