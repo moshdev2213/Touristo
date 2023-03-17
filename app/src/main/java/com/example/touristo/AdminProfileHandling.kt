5d7f724853611a8e2bc036da4da44dc635b3eca2
Mosh <qwwerrrty11@gmail.com>
2023-03-17 16:35:26 +0530

    staffAlmost

---
+++ app/src/main/java/com/example/touristo/AdminProfileHandling.kt
@@ -0,0 +1,11 @@
+package com.example.touristo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AdminProfileHandling : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin_profile_handling)
+    }
+}
