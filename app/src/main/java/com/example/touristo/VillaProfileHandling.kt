259dc719f7212d1383d57821e0231eaf6b0f8440
Mosh <qwwerrrty11@gmail.com>
2023-03-17 18:24:54 +0530

    Villa adding doe

---
+++ app/src/main/java/com/example/touristo/VillaProfileHandling.kt
@@ -0,0 +1,11 @@
+package com.example.touristo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VillaProfileHandling : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_villa_profile_handling)
+    }
+}
