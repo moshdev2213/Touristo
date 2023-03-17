e8d9af7ae4dcd26ffdc817e492c15b9dc3e20326
Mosh <qwwerrrty11@gmail.com>
2023-03-18 02:11:45 +0530

    editVillaDone

---
+++ app/src/main/java/com/example/touristo/VillaEditProfile.kt
@@ -0,0 +1,11 @@
+package com.example.touristo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VillaEditProfile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_villa_edit_profile)
+    }
+}
