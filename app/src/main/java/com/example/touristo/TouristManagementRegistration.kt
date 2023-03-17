3a08f9dd1d6c3ddeea5ac56a1588356db83f8d44
Mosh <qwwerrrty11@gmail.com>
2023-03-17 21:50:11 +0530

    allDoneTourist

---
+++ app/src/main/java/com/example/touristo/TouristManagementRegistration.kt
@@ -0,0 +1,11 @@
+package com.example.touristo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TouristManagementRegistration : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tourist_management_registration)
+    }
+}
