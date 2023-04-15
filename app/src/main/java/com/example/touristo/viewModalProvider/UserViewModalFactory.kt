9990f6bf3d64019962cc0ced07f59eec3a0cc517
Mosh <qwwerrrty11@gmail.com>
2023-04-15 13:35:24 +0530

    theUserViewModelsAdded

--- app/src/main/java/com/example/touristo/viewModalProvider/UserViewModalFactory.kt
+++ app/src/main/java/com/example/touristo/viewModalProvider/UserViewModalFactory.kt
@@ -1,4 +1,17 @@
 package com.example.touristo.viewModalProvider
 
-class UserViewModalFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.touristo.dao.UserDao
+import com.example.touristo.viewModal.UserViewModal
+
+class UserViewModalFactory(
+    private val dao:UserDao
+) :ViewModelProvider.Factory{
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if(modelClass.isAssignableFrom(UserViewModal::class.java)){
+            return UserViewModal(dao) as T
+        }
+        throw java.lang.IllegalArgumentException("Unknown View Model Class")
+    }
 }
