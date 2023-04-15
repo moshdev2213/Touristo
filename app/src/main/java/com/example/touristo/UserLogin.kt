606562693952bbd1159c83b69a743a0e1145e764
Mosh <qwwerrrty11@gmail.com>
2023-04-15 22:50:33 +0530

    UsrLoginFullScreened

--- app/src/main/java/com/example/touristo/UserLogin.kt
+++ app/src/main/java/com/example/touristo/UserLogin.kt
@@ -1,11 +1,23 @@
 package com.example.touristo
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 
 class UserLogin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_login)
+
+        // In Activity's onCreate() for instance this transparents the background
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            val w: Window = window
+            w.setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+            )
+        }
     }
 }
