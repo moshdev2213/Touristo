3f1910d4882fcd5f4208b9de6d4a38b693cc3275
Mosh <qwwerrrty11@gmail.com>
2023-04-16 08:23:10 +0530

    theUiNavsForLoginsSuccess

--- app/src/main/java/com/example/touristo/UserLogin.kt
+++ app/src/main/java/com/example/touristo/UserLogin.kt
@@ -1,12 +1,22 @@
 package com.example.touristo
 
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
 import android.view.WindowManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class UserLogin : AppCompatActivity() {
+    private lateinit var etUloginEmail:EditText
+    private lateinit var etUloginPassword:EditText
+    private lateinit var tvUloginForgotP:TextView
+    private lateinit var tvUloginRegister:TextView
+    private lateinit var tvUloginAdmin:TextView
+    private lateinit var btnUloginSignIn:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_login)
@@ -18,6 +28,19 @@ class UserLogin : AppCompatActivity() {
                 WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
                 WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
             )
+        }
+
+        //coding stuff starts here
+        tvUloginRegister = findViewById(R.id.tvUloginSignup)
+        tvUloginRegister.setOnClickListener {
+            startActivity(Intent(this@UserLogin,UserRegister::class.java))
+            finish()
+        }
+
+        tvUloginAdmin = findViewById(R.id.tvUlogAdmin)
+        tvUloginAdmin.setOnClickListener {
+            startActivity(Intent(this@UserLogin,AdminLogin::class.java))
+            finish()
         }
     }
 }
--- app/src/main/java/com/example/touristo/UserRegister.kt
+++ app/src/main/java/com/example/touristo/UserRegister.kt
@@ -1,11 +1,42 @@
 package com.example.touristo
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class UserRegister : AppCompatActivity() {
+    private lateinit var etURegEmail: EditText
+    private lateinit var etURegPass: EditText
+    private lateinit var etURegRePass: EditText
+    private lateinit var etURegTel: EditText
+    private lateinit var tvURegSignIn: TextView
+    private lateinit var tvURegCpy: TextView
+    private lateinit var btnURegSignUp: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_register)
+
+        // In Activity's onCreate() for instance this transparents the background
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            val w: Window = window
+            w.setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+            )
+        }
+
+        //code starts here
+        tvURegSignIn = findViewById(R.id.tvURegSignIn)
+        tvURegSignIn.setOnClickListener {
+            startActivity(Intent(this@UserRegister,UserLogin::class.java))
+            finish()
+        }
+
     }
 }
